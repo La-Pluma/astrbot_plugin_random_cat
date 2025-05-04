42d6c48c2bde4934246b22405b2f510f34746131
lapluma server <2042287853@qq.com>
2025-05-05 01:53:11 +0800

    v1.0

--- README.md
+++ README.md
@@ -1,9 +1,19 @@
-# helloworld
+# Random Cat
 
-AstrBot 插件模板
+AstrBot 插件 随机猫图
 
-A template plugin for AstrBot plugin feature
+本插件基于TheCatAPI设计开发.
 
-# 支持
+> ![important]
+>
+> 本插件仍在开发
 
-[帮助文档](https://astrbot.app)
+## 命令
+
+`/随机猫图`
+
+## 支持
+
+[Astr帮助文档](https://astrbot.app)
+
+[TheCatAPI](https://thecatapi.com)
--- main.py
+++ main.py
@@ -1,24 +1,48 @@
 from astrbot.api.event import filter, AstrMessageEvent, MessageEventResult
 from astrbot.api.star import Context, Star, register
 from astrbot.api import logger
+import astrbot.api.message_components as Comp
 
-@register("helloworld", "YourName", "一个简单的 Hello World 插件", "1.0.0")
-class MyPlugin(Star):
+import aiohttp
+
+BASE_URL = "https://api.thecatapi.com/v1/images/search"
+
+@register("random_cat", "白星洛", "基于TheCatAPI的AstrBot随机猫图插件", "v1.0")
+class RandomCarPlugin(Star):
     def __init__(self, context: Context):
         super().__init__(context)
 
     async def initialize(self):
         """可选择实现异步的插件初始化方法，当实例化该插件类之后会自动调用该方法。"""
-    
-    # 注册指令的装饰器。指令名为 helloworld。注册成功后，发送 `/helloworld` 就会触发这个指令，并回复 `你好, {user_name}!`
-    @filter.command("helloworld")
-    async def helloworld(self, event: AstrMessageEvent):
-        """这是一个 hello world 指令""" # 这是 handler 的描述，将会被解析方便用户了解插件内容。建议填写。
+
+
+    # 注册指令的装饰器。
+    @filter.command("随机猫图")
+    async def random_cat(self, event: AstrMessageEvent):
+        """随机猫图指令"""
         user_name = event.get_sender_name()
-        message_str = event.message_str # 用户发的纯文本消息字符串
-        message_chain = event.get_messages() # 用户所发的消息的消息链 # from astrbot.api.message_components import *
-        logger.info(message_chain)
-        yield event.plain_result(f"Hello, {user_name}, 你发了 {message_str}!") # 发送一条纯文本消息
+
+        try:
+            # 创建一个 aiohttp ClientSession 实例
+            async with aiohttp.ClientSession() as session:
+                # 发起 GET 请求
+                async with session.get(BASE_URL) as response:
+                    # 检查响应状态码
+                    response.raise_for_status()
+                    
+                    # 解析 JSON 响应
+                    data = await response.json()
+                    # 获取图片 URL
+                    image_url = data[0]['url']
+                    logger.info(f"随机猫图 URL: {image_url}")
+                    yield event.image_result(image_url)
+
+        except aiohttp.ClientError as e:
+            logger.error(f"请求错误: {e}")
+            yield event.plain_result("请求猫咪图片失败，请稍后再试。")
+        except Exception as e:
+            logger.error(f"发生异常: {e}")
+            yield event.plain_result("发生未知错误，请稍后再试。")
 
     async def terminate(self):
         """可选择实现异步的插件销毁方法，当插件被卸载/停用时会调用。"""
